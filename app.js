39680f4ab879fe31c7e94710d778e064fffe67ed
Robert-J-Schaefer <rjschaefer90@gmail.com>
2024-08-19 00:05:17 -0400

    Battle Rounds work

--- app.js
+++ app.js
@@ -10,26 +10,158 @@
 // You win the game if you destroy all of the aliens
 // You lose the game if you are destroyed
 // ------------------------------------------------------------------------------------------------
-let player = {
-    Name:
-};
-let enemy = [];
-//Class for the player, tracks name(maybe), hull(hp), firepower(damage), accuracy
-class Player(name) => {
-    constructor(name){
 
+//A player/Enemy object for class to refer to and furture reassignment of values
+// let playerShip = {
+//     name: "Player 1",
+//     hull: 20,
+//     // maxHull: 20,
+//     // currentHull: 20,
+//     firepower: 5,
+//     accuracy: 0.7,
+//     //Furture ideas for stats
+//     // # of missiles, shield strenght, manuvarability
+// }
+// let enemyShip = {
+//     name: "The Big Bad",
+//     hull: 20,
+//     firepower: 5,
+//     accuracy: .7
+// }
+
+// Keeps Track of the current turn - Odd Player - Even Enemy
+let currentTurn = 1;
+
+//Variable that are beign used
+var cLog = [" ", " ", " ", " ", " "];
+let message = "Attack Hit";
+//Used to check if the last blank in the array has a message in function combatLog()
+let blankChecked = 0;
+
+//Variable to store HTML elements
+let log = document.getElementById('log');
+
+
+//Class for the player, tracks name(maybe), hull(hp), firepower(damage), accuracy
+class Player {
+    constructor(playerShip) {
+        this.name = "Player 1";
+        this.hull = 20;
+        this.firepower = 5;
+        this.accuracy = .7;
+    }
+
+   
+    //Players Lazer Attack the enemy calls hit function and returns damage
+    lazerAtK() {
+        if (this.accuracy >= Math.random(.1, 1).toFixed(1)){
+            player -= this.firepower;
+            message = this.name + "Lazer Hit!";
+            document.getElementById('pHull').innerHTML = this.hull
+            combatLog();
+        }else{
+            message = this.name + "Lazer Missed"
+            combatLog();
+        }
+         
     }
 }
 
-//Class for the enemy, tracks name(maybe), hull(hp), firepower(damage), accuracy
-class Enemy(name) => {
-    constructor(name){
 
+
+//Class for the enemy, tracks name(maybe), hull(hp), firepower(damage), accuracy
+class Enemy {
+    constructor(enemyShip) {
+        this.name = "The Big Bad";
+        this.hull = 20;
+        this.firepower = 5;
+        this.accuracy = .7;
     }
+
+
 }
 
 //Class for controlling a turn. Which include the player and enemy turn
-class GameTurn() =>{
+class GameTurn {
+    // let turn = 1;
+    // playerShip = new Player(player);
+    //Function for running a turn
+    static turn() {
+        // const didHit = hit();
+        if (this.hit() === 1) {
+            //Calc health missing
+        }
+    }
+
 
 }
 
+//Class for assigning enemy stat vaules
+class EnemyStats {
+    //Maybe
+}
+
+// Function to reset game if player chooses to
+function reset() {
+    window.location.reload()
+}
+
+// Function to reset the game
+function gameOver() {
+    message = "GAME OVER! Reset To Play Again";
+    console.log("%c GAME OVER! Reset To Play Again", "background:grey; font-weight:bold; font-size:25px; color:black;")
+    messageEl.textContent = message;
+    btnEl.remove();
+    retreatEl.remove();
+}
+
+//Function to used to change the displayed message in combat log
+function combatLog() {
+    //Loops through an array and assigns the new message to it
+    message = "It works";
+    
+    for (i = 0; i < 5; i++) {
+        //checks to see if all 
+        if (blankChecked > 0){
+            switch (i) {
+                case 0:
+                    cLog[i] = message;
+                    console.log(cLog[i]);
+                    i = 5;
+                    break;
+                case 1:
+                    cLog[i] = message;
+                    console.log(cLog[i]);
+                    break;
+                case 2:
+                    cLog[i] = message;
+                    console.log(cLog[i]);
+                    break;
+                case 3:
+                    cLog[i] = message;
+                    console.log(cLog[i]);
+                    break;
+                case 4:
+                    cLog[i] = message;
+                    console.log(cLog[i]);
+                    blankChecked = 1;
+                    break;
+            }
+        }else{
+            cLog[i] = cLog[i + 1];
+
+            if (i === 4) {
+                cLog[4] = message;
+            }
+        }
+    }
+}
+
+function testing(){
+    combatLog();
+    document.getElementById("log1").innerHTML = cLog[0];
+    document.getElementById("log2").innerHTML = cLog[1];
+    document.getElementById("log3").innerHTML = cLog[2];
+    document.getElementById("log4").innerHTML = cLog[3];
+    document.getElementById("log5").innerHTML = cLog[4];
+}
